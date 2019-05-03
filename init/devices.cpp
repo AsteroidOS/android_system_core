8524e99c6548992b6b43476733bc959215580e77
Florent Revest <revestflo@gmail.com>
2019-05-03 18:32:34 +0200

    init: use /dev/alog/ instead of /dev/log
    Change-Id: I73d26b59bf3b4bccf4156d556204cedfd4eba648

--- init/devices.cpp
+++ init/devices.cpp
@@ -803,7 +803,7 @@ static void handle_generic_device_event(struct uevent *uevent)
          make_dir(base, 0755);
      } else if(!strncmp(uevent->subsystem, "misc", 4) && !strncmp(name, "log_", 4)) {
          LOG(INFO) << "kernel logger is deprecated";
-         base = "/dev/log/";
+         base = "/dev/alog/";
          make_dir(base, 0755);
          name += 4;
      } else
